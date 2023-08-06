d43ce4698ed77af037c342f9f29da33f2a020cf8
XPRESSO-R <73977277+Blaize99@users.noreply.github.com>
2023-08-06 14:04:57 +0530

    Create push.py

---
+++ push.py
@@ -0,0 +1,13 @@
+def sendPush(title, msg, registration_tokens, dataObject=None):
+    message = messaging.MulticastMessage(
+        notification=messaging.Notification(title=title, body=msg),
+        data=dataObject,
+        tokens=registration_tokens,
+    )
+    response = messaging.send_multicast(message)
+    print('Successfully sent message:', response)
+  
+title = "title of the notification"
+msg = "Content of the notification"
+token = "Token of the android device to which the message should be send"
+sendPush(title,msg,token)
