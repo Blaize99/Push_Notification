78e144bd694ae53a01d7d38027a436c527477f7a
XPRESSO-R <73977277+Blaize99@users.noreply.github.com>
2023-08-06 14:05:18 +0530

    Update push.py

--- push.py
+++ push.py
@@ -7,7 +7,7 @@ def sendPush(title, msg, registration_tokens, dataObject=None):
     response = messaging.send_multicast(message)
     print('Successfully sent message:', response)
   
-title = "title of the notification"
+title = "Title of the notification"
 msg = "Content of the notification"
 token = "Token of the android device to which the message should be send"
 sendPush(title,msg,token)
